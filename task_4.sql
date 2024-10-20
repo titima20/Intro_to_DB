e9d43844e46723855a19ee274835fc13a86d9f2f
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-10-20 12:43:12 +0100

    done

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,10 @@
 USE alx_book_store;
 
--- This script prints the full description of the books table
-SHOW TABLE STATUS LIKE 'books';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books'
+ORDER BY ORDINAL_POSITION;
+
 
 
